5e483aef69414cc524e2c826ee24042248f902c0
zycro21 <dimaszycro10@gmail.com>
2025-03-10 18:11:55 +0700

    prepare FE

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+function Home() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-500 mx-64">
+        Welcome to My Website!
+        HALO
+      </h1>
+    </div>
+  );
+}
+
+export default Home;
