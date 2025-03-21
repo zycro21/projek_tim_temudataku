2abb25780a6310a5d5513c9d9264d1bbe6e5a0da
habdil <habdiliqrawardana@gmail.com>
2025-03-21 11:32:11 +0700

    API mentor profile

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -4,6 +4,7 @@ import adminUserRoutes from "./adminUserRoutes";
 import userProfileRoutes from "./userProfileRoutes";
 import roleRoutes from "./roleRoutes";
 import userRoleRoutes from "./userRoleRoutes";
+import mentorProfileRoutes from "./mentorProfileRoutes";
 // Import routes lain di sini
 
 const router = Router();
@@ -24,6 +25,7 @@ router.use('/admin/users', adminUserRoutes);
 router.use('/users', userProfileRoutes);
 router.use('/admin/roles', roleRoutes);
 router.use('/admin/users', userRoleRoutes);
+router.use(mentorProfileRoutes);
 // Tambahkan routes lain seperti:
 // router.use("/users", userRoutes);
 // router.use("/mentors", mentorRoutes);
